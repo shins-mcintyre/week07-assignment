e4700a71672fd6583434106c7f20fc1c8bb1207f
Sinéad McIntyre <shins.mcintyre@gmail.com>
2026-01-17 20:26:25 +0000

    feat: style and layout of form

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -3,8 +3,12 @@
 //TODO: set up a form to collect users' data
 
 import { useState } from "react";
+import { useNavigate } from "react-router";
 
 export default function Form() {
+  // control to go to my posts after form is submitted
+  const navigate = useNavigate();
+
   // 1. Store form data in state
   const [formValues, setFormValues] = useState({
     username: "",
@@ -59,6 +63,8 @@ export default function Form() {
         comments: "",
         src: "",
       });
+      // navigate to my posts page on submission
+      navigate("/my-posts");
     } catch (error) {
       console.error("Error submitting form", error);
     }
@@ -66,78 +72,92 @@ export default function Form() {
 
   // 4. Create the form, with event handler on submit
   return (
-    <>
-      <section className="form-title">
-        <h2>Spotted a cat?</h2>
-        <h3>Don't be selfish, share it with the world!</h3>
-      </section>
-      <form onSubmit={handleFormSubmit}>
-        <label htmlFor="username">Your username:</label>
-        <input
-          type="text"
-          required
-          name="username"
-          value={formValues.username}
-          onChange={handleInputChange}
-        />
+    <section className="cat-form-page">
+      <div className="cat-form-wrapper">
+        <section className="form-title">
+          <h2>Spotted a cat?</h2>
+          <h3>Share it with the world!</h3>
+        </section>
+        <form className="cat-form" onSubmit={handleFormSubmit}>
+          <div className="form-row">
+            <label htmlFor="username">Your username:</label>
+            <input
+              type="text"
+              required
+              name="username"
+              value={formValues.username}
+              onChange={handleInputChange}
+            />
+          </div>
 
-        <label htmlFor="date">Date of sighting:</label>
-        <input
-          type="date"
-          required
-          name="date"
-          value={formValues.date}
-          onChange={handleInputChange}
-        />
+          <div className="form-row">
+            <label htmlFor="date">Date of sighting:</label>
+            <input
+              type="date"
+              required
+              name="date"
+              value={formValues.date}
+              onChange={handleInputChange}
+            />
+          </div>
 
-        <label htmlFor="location">Location of sighting:</label>
-        <input
-          type="text"
-          required
-          name="location"
-          value={formValues.location}
-          onChange={handleInputChange}
-        />
+          <div className="form-row">
+            <label htmlFor="location">Location of sighting:</label>
+            <input
+              type="text"
+              required
+              name="location"
+              value={formValues.location}
+              onChange={handleInputChange}
+            />
+          </div>
 
-        <label htmlFor="approach_score">Approachability score:</label>
-        <input
-          type="number"
-          required
-          name="approach_score"
-          min="1"
-          max="5"
-          value={formValues.approach_score}
-          onChange={handleInputChange}
-        />
+          <div className="form-row">
+            <label htmlFor="approach_score">Approachability score:</label>
+            <input
+              type="number"
+              required
+              name="approach_score"
+              min="1"
+              max="5"
+              value={formValues.approach_score}
+              onChange={handleInputChange}
+            />
+          </div>
 
-        <label htmlFor="comments">Comments:</label>
-        <input
-          type="text"
-          name="comments"
-          value={formValues.comments}
-          onChange={handleInputChange}
-        />
+          <div className="form-row">
+            <label htmlFor="comments">Comments:</label>
+            <input
+              type="text"
+              name="comments"
+              value={formValues.comments}
+              onChange={handleInputChange}
+            />
+          </div>
 
-        <label htmlFor="src">Image link:</label>
-        <input
-          type="url"
-          required
-          name="src"
-          value={formValues.src}
-          onChange={handleInputChange}
-        />
+          <div className="form-row">
+            <label htmlFor="src">Image link:</label>
+            <input
+              type="url"
+              required
+              name="src"
+              value={formValues.src}
+              onChange={handleInputChange}
+            />
+          </div>
 
-        <button type="submit">Submit</button>
-      </form>
+          <button type="submit">Submit</button>
+        </form>
 
-      {/* test if it works */}
-      {/* <p>Username: {formValues.username}</p>
+        {/* test if it works */}
+        {/* <p>Username: {formValues.username}</p>
       <p>Date: {formValues.date}</p>
       <p>Location: {formValues.location}</p>
       <p>Approachability score: {formValues.approach_score}</p>
       <p>Comments:{formValues.comments}</p>
       <p>Link: {formValues.src}</p> */}
-    </>
+      </div>
+    </section>
   );
 }
 
